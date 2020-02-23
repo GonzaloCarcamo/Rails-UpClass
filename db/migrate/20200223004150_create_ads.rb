24b4d0270e8627c9c9c3b8bb41286879556eb07e
GonzaloCarcamo <gon.cmo.dev@gmail.com>
2020-02-22 21:43:57 -0300

    creación scaffold ad, creación y migración DB

---
+++ db/migrate/20200223004150_create_ads.rb
@@ -0,0 +1,11 @@
+class CreateAds < ActiveRecord::Migration[5.2]
+  def change
+    create_table :ads do |t|
+      t.string :title
+      t.string :category
+      t.string :tag
+
+      t.timestamps
+    end
+  end
+end
