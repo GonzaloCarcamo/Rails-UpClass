baf086894b31fbdea3ad0e83b94a1b76578e7afe
GonzaloCarcamo <gon.cmo.dev@gmail.com>
2020-02-22 23:03:00 -0300

    instalación y configuración de gema devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_23_010132) do
+ActiveRecord::Schema.define(version: 2020_02_23_015435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2020_02_23_010132) do
   create_table "ads_tags", id: false, force: :cascade do |t|
     t.bigint "ad_id", null: false
     t.bigint "tag_id", null: false
+    t.index ["ad_id", "tag_id"], name: "index_ads_tags_on_ad_id_and_tag_id"
+    t.index ["tag_id", "ad_id"], name: "index_ads_tags_on_tag_id_and_ad_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -38,6 +40,21 @@ ActiveRecord::Schema.define(version: 2020_02_23_010132) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name"
+    t.string "last_name"
+    t.boolean "admin", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
